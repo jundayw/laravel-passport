74ee32d67ba82b60594b78c5c2ccbdbd16d5afe7
jundayw <jundayw@126.com>
2026-09-24 18:52:47 +0800

    wip:Model/Passport

--- src/Contracts/Model/Passport.php
+++ src/Contracts/Model/Passport.php
@@ -2,7 +2,20 @@
 
 namespace Jundayw\Passport\Contracts\Model;
 
+use Jundayw\Passport\Exceptions\PassportDisabledException;
+use Jundayw\Passport\Exceptions\PassportNotFoundException;
+
 interface Passport
 {
-
+    /**
+     * Retrieve the secret associated with the given key.
+     *
+     * @param string $key The identifier of the passport entry
+     *
+     * @return string The secret value
+     *
+     * @throws PassportNotFoundException If no model is found for the given key
+     * @throws PassportDisabledException If the found model has a 'disable' state
+     */
+    public function getSecret(string $key): string;
 }
--- src/Model/Passport.php
+++ src/Model/Passport.php
@@ -6,7 +6,10 @@ use DateTimeInterface;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Cache;
 use Jundayw\Passport\Contracts\Model\Passport as PassportModel;
+use Jundayw\Passport\Exceptions\PassportDisabledException;
+use Jundayw\Passport\Exceptions\PassportNotFoundException;
 
 class Passport extends Model implements PassportModel
 {
@@ -99,5 +102,64 @@ class Passport extends Model implements PassportModel
     protected function serializeDate(DateTimeInterface $date): string
     {
         return $date->format($this->dateFormat);
+    }
+
+    /**
+     * Retrieve the secret associated with the given key.
+     *
+     * @param string $key The identifier of the passport entry
+     *
+     * @return string The secret value
+     *
+     * @throws PassportNotFoundException If no model is found for the given key
+     * @throws PassportDisabledException If the found model has a 'disable' state
+     */
+    public function getSecret(string $key): string
+    {
+        $passport = $this->resolvePayload($key);
+
+        if (is_null($passport)) {
+            throw new PassportNotFoundException(
+                sprintf('Model not found for key: %s', $key)
+            );
+        }
+
+        if ($passport->getAttribute('state') === 'disable') {
+            throw new PassportDisabledException(
+                sprintf('Model is disabled for key: %s', $key)
+            );
+        }
+
+        return $passport->getAttribute('secret');
+    }
+
+    /**
+     * Fetch the passport model from cache, or from the database and store it in the cache.
+     *
+     * @param string $key The identifier to look up
+     *
+     * @return Model|null The model instance if found, otherwise null
+     */
+    protected function resolvePayload(string $key): ?Model
+    {
+        $sentinel = static::class;
+        $cached   = Cache::remember(
+            "passport:{$key}",
+            now()->addSeconds(config('passport.ttl.resolved')),
+            function () use ($key, $sentinel) {
+                $passport = $this->where(['key' => $key])->first();
+                if (is_null($passport)) {
+                    Cache::put(
+                        "passport:{$key}",
+                        $sentinel,
+                        config('passport.ttl.fallback')
+                    );
+                    return $sentinel;
+                }
+                return $passport;
+            }
+        );
+
+        return $cached === $sentinel ? null : $cached;
     }
 }
